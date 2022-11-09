f26717aaf2ecb74aa4149b3fb83e75a12d832ec2
iso <ismailkurtlojistik@gmail.com>
2022-11-09 14:57:11 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import AuthContextProvider from "./context/AuthContextProvider";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+  return (
+    <div className="bg-grey-100  dark:bg-[#23242a]">
+      <AuthContextProvider>
+        <AppRouter />
+        <ToastContainer />
+      </AuthContextProvider>
+    </div>
+  );
+};
+
+export default App;
